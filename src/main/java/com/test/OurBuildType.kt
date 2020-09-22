2d420a67f61d7ab101411d3c15b6d458667dda90
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2020-09-22 19:56:59 +0200

    fix custom build type definition

--- src/main/java/com/test/OurBuildType.kt
+++ src/main/java/com/test/OurBuildType.kt
@@ -2,9 +2,11 @@ package com.test
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 
-open class OurBuildType : BuildType ({
+open class OurBuildType(init: BuildType.() -> Unit) : BuildType ({
 
+    init()
     params {
         param("env.someParam", "111")
     }
 })
+
