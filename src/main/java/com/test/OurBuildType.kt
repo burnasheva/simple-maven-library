6c63d889e2d498ff049c5d11af58daceee15f0f5
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2020-09-22 14:37:31 +0200

    one more try

--- src/main/java/com/test/OurBuildType.kt
+++ src/main/java/com/test/OurBuildType.kt
@@ -2,10 +2,9 @@ package com.test
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 
-open class OurBuildType : BuildType ({
+open class OurBuildType constructor(init: BuildType.() -> kotlin.Unit) : BuildType ({
 
     params {
         param("env.someParam", "111")
     }
-
 })
