a122b68c8748ca511b15e4ad9c0052a155a0e435
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2020-09-22 15:24:29 +0200

    removing constructor

--- src/main/java/com/test/OurBuildType.kt
+++ src/main/java/com/test/OurBuildType.kt
@@ -2,7 +2,7 @@ package com.test
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 
-open class OurBuildType constructor(init: BuildType.() -> kotlin.Unit) : BuildType ({
+open class OurBuildType : BuildType ({
 
     params {
         param("env.someParam", "111")
