2468f9bb26c68b3316717a463b131deedc78b99f
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2021-01-20 19:58:50 +0100

    multi line comment

--- src/main/java/com/test/OurBuildType.kt
+++ src/main/java/com/test/OurBuildType.kt
@@ -6,7 +6,7 @@ open class OurBuildType(init: BuildType.() -> Unit) : BuildType ({
 
     init()
     params {
-        param("env.someParam", "000")
+        param("env.someParam", "111")
     }
 })
 
